2dbea64ecd5f64ac1d3a7f4d80c6736d4bd280ef
Sam Kester <samkester@gmail.com>
2020-09-22 10:56:20 -0600

    add'l Redux comments

--- src/components/ViewProjects.js
+++ src/components/ViewProjects.js
@@ -3,7 +3,7 @@ import AddProject from '../utils/EditProject';
 import Projects from '../utils/Projects';
 import axiosWithAuth from '../utils/axiosWithAuth';
 import {FormField, LoginForm} from './styled-components';
-import { postTo } from '../store/actions';
+import { postTo, putTo, deleteFrom } from '../store/actions';
 
 function ProjectList ({projects}) { // props here are linked by connect() to the Redux store
     //const [trigger, setTrigger] = useState(false); // Redux state is global, so will automatically trigger re-renders where appropriate
@@ -36,10 +36,16 @@ function ProjectList ({projects}) { // props here are linked by connect() to the
     )
 }
 
+// connect() tells the Redux provider what state we want to access in each component
 export default connect((state) => { return {
-    //props
+    //props - these are parts of the state object we want to access. They are shared among all components.
+    // State is defined in `store/reducers/index.js` and includes projects, students, and reminders arrays
     projects: state.projects,
 }},{
-    //actionMakers
+    //actionMakers - these are actions we can use that will affect state for all components.
+    // Actions are defined in `store/actions/index.js` and include getInitialData, postTo, putTo, and deleteFrom that wrap their
+    //   respective HTTP actions and put the results in state.
     postTo,
+    putTo,
+    deleteFrom,
 })(ProjectList);
